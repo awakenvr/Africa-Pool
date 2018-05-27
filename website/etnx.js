0c17ef5fbcd96518e91c88172970b076a1c91c43
awakenvr <39466459+awakenvr@users.noreply.github.com>
2018-05-27 19:13:40 +0200

    update

---
+++ website/etnx.js
@@ -0,0 +1,17 @@
+var api = "//169.239.183.32:8117";
+var coinUnits = 100;
+var poolHost = "afpool.electronero.org";
+
+var irc = "irc.freenode.net/#etnx-pools";
+
+var email = "support@support.etnx.pool";
+
+var cryptonatorWidget = ["{symbol}-BTC", "{symbol}-USD", "{symbol}-EUR"];
+
+var easyminerDownload = "#";
+
+var blockchainExplorer = "https://blockexplorer.electronero.org/block/{id}";
+
+var transactionExplorer = "https://blockexplorer.electronero.org/tx/{id}";
+
+var themeCss = "themes/default.css";
